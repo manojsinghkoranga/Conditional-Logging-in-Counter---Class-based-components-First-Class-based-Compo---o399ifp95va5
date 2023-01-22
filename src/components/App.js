ae5df5f3e3fa5f06ad70ce25cec462ab9e43d58d
manoj singh <117524976+manojsinghkoranga@users.noreply.github.com>
2023-01-22 18:18:19 +0000

    copied

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,21 @@ import React from 'react'
 import '../styles/App.css';
 
 class App extends React.Component{
-  
-  handleClick(){
-    this.setState({count: this.state.count + 1})
-  }
   constructor(props){
     super(props)
     this.state = {count:0}
-    this.handleClick = this.handleClick.bind(this)
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    const isCountEven = nextState.count % 2 === 0;
+    if (isCountEven) {
+      return true;
+    }
+    return false;
+  }
+
+  handleClick = () => {
+    this.setState({count: this.state.count + 1})
   }
 
 
@@ -21,7 +28,6 @@ class App extends React.Component{
         <button id="incr-btn" onClick={this.handleClick}>Increment</button>
       </div>
     )
-
   }
 }
 
